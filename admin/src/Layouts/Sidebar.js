d7f01cff04dfe0566de399dee4333535316efe4d
Fabricejor <Fabricejordan2001@gmail.com>
2024-05-01 22:51:13 +0000

    update initialisation du dashboard admin

--- admin/src/Layouts/Sidebar.js
+++ admin/src/Layouts/Sidebar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "../Assets/Styles/sidebar.css"
+import { FaArrowsAltH } from "react-icons/fa";
+import { IconName } from "react-icons/fa6";
 
 export default function Sidebar() {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </div>
+    <>
+        <div className='flex'>
+            <div className='bg-green-dark h-screen p-5 pt-8 w-72'>
+            <FaArrowsAltH className="bg-white text-green-dark text-rounded"/>
+                Sidebar</div>
+            <div className='p-7'> <h1 className='text-2xl font-semibold'>Home</h1></div>
+        </div>   
+    </>
   )
 }
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/Layouts/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "green-dark":"#00e686",
+        "green-medium":"#80ffca",
+        "green-light":"#cdffea",
+        "blue-light":"#cdf2ff",
+        "blue-medium":"#80ddff",
+        "blue-dark":"#00a9e6"
+      }
+    },
   },
   plugins: [],
 }
